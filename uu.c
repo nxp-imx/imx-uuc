0c156a78e2ec3820ecdf07817fc4e8401cac63f9
Allen Xu <b45815@freescale.com>
2014-03-21 14:38:38 -0500

    ENGR00304791-3 update license header
    From: Otavio Salvador <otavio@ossystems.com.br>
    Date: Fri, 17 Jan 2014 09:34:31 -0200
    Subject: uu.c: update license header
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
    
    Signed-off-by: Allen Xu <b45815@freescale.com>

--- uu.c
+++ uu.c
@@ -1,7 +1,7 @@
 /*
  * iMX utp decode program
  *
- * Copyright 2010-2013 Freescale Semiconductor, Inc.
+ * Copyright (C) 2010-2014 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,9 +13,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #define _GNU_SOURCE

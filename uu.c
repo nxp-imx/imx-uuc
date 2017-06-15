f8d0c75226f3ebfed46c903d779d319057225f87
Michael Heimpold <mhei@heimpold.de>
2017-06-15 21:56:14 +0200

    uu.c: fix compiler warnings for unused variables
    This fixes:
    
    uu.c: In function ‘utp_mk_devnode’:
    uu.c:119:8: warning: unused variable ‘colon’ [-Wunused-variable]
      char *colon;
            ^
    
    uu.c: In function ‘utp_run’:
    uu.c:150:6: warning: unused variable ‘r’ [-Wunused-variable]
      int r;
          ^
    
    uu.c: In function ‘utp_pipe’:
    uu.c:313:6: warning: unused variable ‘r’ [-Wunused-variable]
      int r, infp;
          ^
    
    uu.c: In function ‘main’:
    uu.c:689:8: warning: unused variable ‘thread_result’ [-Wunused-variable]
      void *thread_result;
            ^
    
    uu.c:687:7: warning: unused variable ‘env’ [-Wunused-variable]
      char env[256];
           ^
    
    uu.c: At top level:
    uu.c:89:14: warning: ‘utp_file_f’ defined but not used [-Wunused-variable]
     static FILE *utp_file_f = NULL;
                  ^
    
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

--- uu.c
+++ uu.c
@@ -86,7 +86,6 @@ struct utp_message {
 #pragma pack()
 
 static int utp_file = -1;
-static FILE *utp_file_f = NULL;
 
 static inline char *utp_answer_type(struct utp_message *u)
 {
@@ -116,7 +115,6 @@ static int utp_mk_devnode(char *class, char *name, char *node, int type)
 	char sys[256];
 	char devnode[20]; /* major:minor */
 	int major, minor;
-	char *colon;
 	int len, f, rc = -EINVAL;
 
 	if (access(node, F_OK) == 0) {
@@ -147,7 +145,6 @@ static int utp_mk_devnode(char *class, char *name, char *node, int type)
  */
 static int utp_run(char *command, ... )
 {
-	int r;
 	char cmd[1024];
 	va_list vptr;
 
@@ -310,7 +307,7 @@ pid_t popen2(const char *command, int *infp, int *outfp)
 }
 int utp_pipe(char *command, ... )
 {
-	int r, infp;
+	int infp;
 	char shell_cmd[1024];
 	va_list vptr;
 	va_start(vptr, command);
@@ -684,9 +681,7 @@ int main(void)
 	int watchdog_timeout = 127;  /* sec */
 	int cpu_id = 50;
 	struct utp_message *uc, *answer;
-	char env[256];
 	pthread_t a_thread;
-	void *thread_result;
 
 	printf("%s %s [built %s %s]\n", PACKAGE, VERSION, __DATE__, __TIME__);
 	/* set stdout unbuffered, what is the usage??? */

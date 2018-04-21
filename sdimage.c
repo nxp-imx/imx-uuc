e8e70c15c83dfe669eeafb72d8b8605a76b6c0a1
Michael Heimpold <mhei@heimpold.de>
2018-04-21 23:42:29 +0200

    sdimage: refactor boot control block structure
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

--- sdimage.c
+++ sdimage.c
@@ -45,20 +45,30 @@ struct mbr {
 	uint16_t signature;
 } __attribute__ ((packed));
 
-struct DeviceInfo {
-	unsigned int u32ChipNum;
-	unsigned int u32DriverType;
-	unsigned int u32Tag;
-	unsigned int u32FirstSectorNumber;
-	unsigned int u32SectorCount;
+/* Drive Info Data Structure */
+struct drive_info {                         /* Comments from i.MX28 RM:                    */
+	uint32_t chip_num;                  /* chip select, ROM does not use it            */
+	uint32_t drive_type;                /* always system drive, ROM does not use it    */
+	uint32_t tag;                       /* drive tag                                   */
+	uint32_t first_sector_number;       /* start sector/block address of firmware      */
+	uint32_t sector_count;              /* not used by ROM                             */
 } __attribute__ ((packed));
 
-struct ConfigBlock {
-	unsigned int u32Sigature;
-	unsigned int u32PrimaryBootTag;
-	unsigned int u32SecondaryBootTags;
-	unsigned int u32NumCopies;
-	struct DeviceInfo aDriverInfo[10];
+/* (maximum) elements in following drive info array
+ * It's a design decision that this tool only supports two elements (at the moment)
+ */
+#define MAX_DI_COUNT 2
+
+/* Boot Control Block (BCB) Data Structure */
+struct bcb {                                /* (Analogous) Comments from i.MX28 RM:        */
+	uint32_t signature;                 /* signature 0x00112233                        */
+	uint32_t primary_boot_tag;          /* primary boot drive identified by this tag   */
+	uint32_t secondary_boot_tag;        /* secondary boot drive identified by this tag */
+	uint32_t num_copies;                /* num elements in drive_info array            */
+	struct drive_info drive_info[MAX_DI_COUNT];
+	                                    /* let drive_info array be last in this data
+	                                     * structure to be able to add more drives in
+	                                     * future without changing ROM code            */
 } __attribute__ ((packed));
 
 int main(int argc, char **argv)
@@ -67,7 +77,7 @@ int main(int argc, char **argv)
 	int devhandle;
 	int firmwarehandle;
 	struct mbr mbr;
-	struct ConfigBlock bcb;
+	struct bcb bcb;
 	char *buff;
 	struct stat filestat;
 	int mincount;
@@ -144,21 +154,21 @@ int main(int argc, char **argv)
 	}
 
 	memset(&bcb, 0, sizeof(bcb));
-	bcb.u32Sigature = 0x00112233;
-	bcb.u32PrimaryBootTag = 1;
-	bcb.u32SecondaryBootTags = 2;
-	bcb.u32NumCopies = 2;
+	bcb.signature = 0x00112233;
+	bcb.primary_boot_tag = 1;
+	bcb.secondary_boot_tag = 2;
+	bcb.num_copies = 2;
 
-	bcb.aDriverInfo[0].u32ChipNum = 0;
-	bcb.aDriverInfo[0].u32DriverType = 0;
-	bcb.aDriverInfo[0].u32Tag = bcb.u32PrimaryBootTag;
-	bcb.aDriverInfo[0].u32FirstSectorNumber = mbr.partition[i].start + 4;
+	bcb.drive_info[0].chip_num = 0;
+	bcb.drive_info[0].drive_type = 0;
+	bcb.drive_info[0].tag = bcb.primary_boot_tag;
+	bcb.drive_info[0].first_sector_number = mbr.partition[i].start + 4;
 
-	bcb.aDriverInfo[1].u32ChipNum = 0;
-	bcb.aDriverInfo[1].u32DriverType = 0;
-	bcb.aDriverInfo[1].u32Tag = bcb.u32SecondaryBootTags;
-	bcb.aDriverInfo[1].u32FirstSectorNumber =
-	    bcb.aDriverInfo[0].u32FirstSectorNumber
+	bcb.drive_info[1].chip_num = 0;
+	bcb.drive_info[1].drive_type = 0;
+	bcb.drive_info[1].tag = bcb.secondary_boot_tag;
+	bcb.drive_info[1].first_sector_number =
+	    bcb.drive_info[0].first_sector_number
 	    + ((filestat.st_size + 511) / 512);
 
 	lseek(devhandle, mbr.partition[i].start * 512, SEEK_SET);
@@ -180,7 +190,7 @@ int main(int argc, char **argv)
 
 	printf("write first firmware\n");
 
-	lseek(devhandle, bcb.aDriverInfo[0].u32FirstSectorNumber * 512, SEEK_SET);
+	lseek(devhandle, bcb.drive_info[0].first_sector_number * 512, SEEK_SET);
 	if (write(devhandle, buff, filestat.st_size) != filestat.st_size) {
 		printf("first firmware write fail\n");
 		return -1;
@@ -188,7 +198,7 @@ int main(int argc, char **argv)
 
 	printf("write second firmware\n");
 
-	lseek(devhandle, bcb.aDriverInfo[1].u32FirstSectorNumber * 512, SEEK_SET);
+	lseek(devhandle, bcb.drive_info[1].first_sector_number * 512, SEEK_SET);
 	if (write(devhandle, buff, filestat.st_size) != filestat.st_size) {
 		printf("second firmware write fail\n");
 		return -1;
